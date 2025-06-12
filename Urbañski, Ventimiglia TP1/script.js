d538fc04785ae6e8aeee0f773d431a035a453ea8
ianurbanski <166758728+ianurbanski@users.noreply.github.com>
2025-06-12 18:18:37 -0300

    Add files via upload

--- Urbañski, Ventimiglia TP1/script.js
+++ Urbañski, Ventimiglia TP1/script.js
@@ -1,45 +1,4 @@
 
-/*seccion tarjetas (opiniones)*/
-
-
-function ajustarEstilos() {
-    let contenedor = document.querySelector(".contenedor");
-    let tarjeta = document.getElementById("tarjeta");
-    let contenidoTarjeta = document.querySelector(".contenidoTarjeta");
-
-    // Verificar si los elementos existen antes de aplicar estilos
-    if (contenedor) {
-        if (window.innerWidth <= 550) {
-            contenedor.style.flexDirection = "column";
-            contenedor.style.gap = "20px";
-        } else {
-            contenedor.style.flexDirection = "row";
-            contenedor.style.gap = "300px";
-        }
-    }
-
-    if (tarjeta && contenidoTarjeta) {
-        if (window.innerWidth <= 550) {
-            tarjeta.style.width = "90%";
-            tarjeta.style.height = "auto";
-            tarjeta.style.padding = "20px";
-            tarjeta.style.margin = "10px auto";
-            tarjeta.style.backgroundSize = "contain"; /* Para que el fondo no recorte texto */
-
-            contenidoTarjeta.style.flexDirection = "column";
-            contenidoTarjeta.style.alignItems = "center";
-        } else {
-            tarjeta.style.width = "90vw"; /* Evita que se corte en pantallas grandes */
-            tarjeta.style.height = "auto";
-            tarjeta.style.padding = "30px 10px 150px 150px";
-            tarjeta.style.backgroundSize = "cover";
-
-            contenidoTarjeta.style.flexDirection = "row";
-        }
-    }
-}
-
-
 /*seccion boton volver arriba*/
 
 
@@ -64,7 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if (formulario.checkValidity()) {
       
       const nombre = document.getElementById("nombreYApellido").value;
+      const email = document.getElementById("email").value;
+
       localStorage.setItem("nombreUsuario", nombre);
+      localStorage.setItem("emailUsuario", email);
 
       // Renvia a pantalla gracias
       window.location.href = "gracias.html";
